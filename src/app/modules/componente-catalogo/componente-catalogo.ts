c01e8c004e6ec6df41cba7f91a89a5ab1fdd96dc
cristian-2904-monroy <cristian2904monroy@gmail.com>
2026-05-20 12:33:19 -0500

    Corregir carga del catálogo: forzar detección de cambios y agregar spinner

--- src/app/modules/componente-catalogo/componente-catalogo.ts
+++ src/app/modules/componente-catalogo/componente-catalogo.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 import { CamionesService } from '../../services/camiones.service';
 
 @Component({
@@ -11,30 +10,34 @@ import { CamionesService } from '../../services/camiones.service';
   templateUrl: './componente-catalogo.html',
   styleUrl: './componente-catalogo.css',
 })
-
 export class ComponenteCatalogo implements OnInit {
-
   catalogo: any[] = [];
+  cargando = true;  // Para mostrar un spinner o texto
 
   constructor(
-    private camionesService: CamionesService
+    private camionesService: CamionesService,
+    private cdr: ChangeDetectorRef  // Inyectar ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
-
-    this.camionesService
-      .obtenerCamiones()
-      .subscribe((camiones: any[]) => {
-
+    this.cargando = true;
+    this.camionesService.obtenerCamiones().subscribe({
+      next: (camiones: any[]) => {
+        console.log('Camiones recibidos:', camiones);
         this.catalogo = [
           {
             titulo: 'Catálogo de Camiones',
             listado: camiones
           }
         ];
-
-      });
-
+        this.cargando = false;
+        this.cdr.detectChanges();  // 🔄 Forzar actualización de la vista
+      },
+      error: (err) => {
+        console.error('Error al obtener camiones:', err);
+        this.cargando = false;
+        this.cdr.detectChanges();
+      }
+    });
   }
-
 }
