249043d393228cc8c0f9e45cd895da4f618a2392
Javier sebastian Salazar fuentes <javiersebastiansalazar16@gmail.com>
2026-05-28 23:04:29 -0500

    Update nav-button.ts

--- src/app/components/nav-button/nav-button.ts
+++ src/app/components/nav-button/nav-button.ts
@@ -1,27 +1,13 @@
 import { Component, input } from '@angular/core';
-// Importa Component para crear componentes Angular.
-// También trae input, utilizado para recibir datos desde otros componentes.
-
 import { RouterModule } from '@angular/router';
-// Importa las herramientas de enrutamiento necesarias para navegar entre páginas.
+
 
 @Component({
-  // Configura las propiedades principales del componente.
-
   selector: 'app-nav-button',
-  // Define la etiqueta HTML utilizada para insertar este botón en otras vistas.
-
   standalone: true,
-  // Permite que el componente funcione sin depender de un módulo Angular tradicional.
-
   imports: [RouterModule],
-  // Habilita las directivas de navegación como routerLink dentro del HTML.
-
   templateUrl: './nav-button.html',
-  // Conecta el archivo que contiene la estructura visual del botón.
-
   styleUrl: './nav-button.css'
-  // Vincula los estilos personalizados aplicados al componente.
 })
 
 export class NavButton {
