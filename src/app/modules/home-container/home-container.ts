80b3a17e2c2ec4c559fdcc5f02543a2eee9d7d4d
Javier sebastian Salazar fuentes <javiersebastiansalazar16@gmail.com>
2026-05-28 23:37:39 -0500

    Update home-container.ts

--- src/app/modules/home-container/home-container.ts
+++ src/app/modules/home-container/home-container.ts
@@ -1,24 +1,17 @@
 import { Component } from '@angular/core';
-// Importa el decorador Component para definir un componente en Angular
 
 import { Router } from '@angular/router';
-// Importa el servicio Router para poder navegar entre rutas
 
 @Component({
   selector: 'app-home-container',
-  // Nombre del selector que se usará en el HTML para este componente
 
   standalone: true,
-  // Indica que es un componente independiente (no depende de módulos)
 
   imports: [],
-  // Aquí se incluirían otros módulos o componentes si fueran necesarios (en este caso está vacío)
 
   templateUrl: './home-container.html',
-  // Archivo HTML que define la vista del componente
 
   styleUrl: './home-container.css',
-  // Archivo CSS con los estilos del componente
 })
 export class HomeContainer {
   // Clase principal del componente Home
