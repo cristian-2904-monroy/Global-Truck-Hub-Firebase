da4b9c06efa7024c40d7317bcc6ce51153da0268
Javier sebastian Salazar fuentes <javiersebastiansalazar16@gmail.com>
2026-05-28 23:09:23 -0500

    Update tarjeta-camion.ts

--- src/app/components/tarjeta-camion/tarjeta-camion.ts
+++ src/app/components/tarjeta-camion/tarjeta-camion.ts
@@ -1,28 +1,19 @@
 import { Component, input } from '@angular/core';
-// Importa Component para crear el componente.
-// También trae input, utilizado para recibir datos desde otros componentes.
 
 import { CurrencyPipe } from '@angular/common';
-// Importa el pipe encargado de formatear valores numéricos como monedas.
 
 import { Router } from '@angular/router';
-// Trae el servicio Router para permitir la navegación entre páginas.
 
 @Component({
   selector: 'app-tarjeta-camion',
-  // Define la etiqueta HTML utilizada para insertar esta tarjeta en otras vistas.
 
   standalone: true,
-  // Permite que el componente funcione sin depender de un módulo Angular tradicional.
 
   imports: [CurrencyPipe],
-  // Registra el pipe de moneda para utilizarlo directamente dentro del HTML.
 
   templateUrl: './tarjeta-camion.html',
-  // Conecta el archivo que contiene la estructura visual de la tarjeta.
 
   styleUrl: './tarjeta-camion.css'
-  // Vincula la hoja de estilos encargada del diseño visual del componente.
 })
 export class TarjetaCamion {
 
